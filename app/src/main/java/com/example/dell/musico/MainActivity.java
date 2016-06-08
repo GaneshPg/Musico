c3ffc2dad71ec0407d13fc728b9db5696ec0d072
GaneshPg <pgshriganesh@gmail.com>
2016-06-08 23:20:54 +0530

    Add scan directory for music files

--- app/src/main/java/com/example/dell/musico/MainActivity.java
+++ app/src/main/java/com/example/dell/musico/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.dell.musico;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnClickListener{
 
     private ImageView playBtn, nextBtn, prevBtn;
+    private List<String> songFiles;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getMusicFiles(Environment.getExternalStorageDirectory());
+        mediaPlayer = new MediaPlayer();
+
+        List<String> songFiles = new ArrayList<String>();
 
         playBtn = (ImageView) findViewById(R.id.playPauseButton);
         nextBtn = (ImageView) findViewById(R.id.nextButton);
@@ -36,5 +48,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 //Play previous song
                 break;
         }
+    }
+
+    public void getMusicFiles(File file) {
+        if (file.isDirectory()) {
+            for (File filename : file.listFiles()) {
+                if (filename.isDirectory())
+                    getMusicFiles(filename);
+                else if (filename.isFile() && filename.toString().endsWith(".mp3") || filename.toString().endsWith(".wav"))
+                    songFiles.add(filename.getAbsolutePath());
+            }
+        } else if (file.isFile() && file.toString().endsWith(".mp3") || file.toString().endsWith(".wav"))
+            songFiles.add(file.getAbsolutePath());
     }
 }
